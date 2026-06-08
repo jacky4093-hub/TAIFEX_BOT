38560e8a869d6c40e83ca6ac5fd18b30f85ff30b
jacky4093 <jacky4093@gmail.com>
2026-06-08 10:03:32 +0800

    556

--- Github_同步.bat
+++ Github_同步.bat
@@ -1,13 +1,14 @@
 @echo off
-
 cd /d "%~dp0"
+
+git config --global user.name "jacky4093"
+git config --global user.email "jacky4093@gmail.com"
 
 git pull origin main
 
 git add .
 
 set /p MSG=請輸入更新說明:
-
 if "%MSG%"=="" set MSG=update
 
 git commit -m "%MSG%"
